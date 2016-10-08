86130834aed361d85886fe209ca8493853967877
fenghou <johnhu456@163.com>
2016-10-08 16:01:43 +0800

    1、初始化部分cards的数据

---
+++ officalDemo/page/Cards/index/data/card_list_name.js
@@ -0,0 +1,18 @@
+var data = {
+    "code" : "0",
+    "cards":{
+        "A":[
+            1,2,3,4
+        ],
+        "B":[
+            1,2,3,4
+        ],
+        "C":[
+            1,2,3,4
+        ],
+    }
+}
+
+module.exports = {
+    data:data
+}
--- officalDemo/page/Cards/index/index.js
+++ officalDemo/page/Cards/index/index.js
@@ -1,16 +1,19 @@
+
+var card_list_name = require("data/card_list_name.js");
+var nameData;
 Page({
   data:{
-     id:0,
-     count:1,
-     message:"这是一个页面",
-     array:[1,2,3,4,5],
-     view:"fuck",
-     staffA:{firstName:'AfirstName',lastName:'AlastName'},
-     staffB:{firstName:'BfirstName',lastName:'BlastName'},
-     staffC:{firstName:'CfirstName',lastName:'ClastName'},
-     a:1,
-     b:2,
-     c:3,
+    //  id:0,
+    //  count:1,
+    //  message:"这是一个页面",
+    //  array:[1,2,3,4,5],
+    //  view:"fuck",
+    //  staffA:{firstName:'AfirstName',lastName:'AlastName'},
+    //  staffB:{firstName:'BfirstName',lastName:'BlastName'},
+    //  staffC:{firstName:'CfirstName',lastName:'ClastName'},
+    //  a:1,
+    //  b:2,
+    //  c:3,
   },
   bindInputChange:function(e){
       console.log('13234')
@@ -23,12 +26,31 @@ Page({
   bindButtonTapSheet:function(e){
       console.log('tap')
   },
-  onLoad:function(options){
+  onLoad:function(){
     // 页面初始化 options为页面跳转所带来的参数
+    // 排序字母显示
+    console.log(card_list_name)
+    var data1 = card_list_name.data.cards
+    nameData = []
+    for(var k in data1){
+      var group = {}
+      group.name = k
+      group.cards = data1[k]
+      for(var i in group.cards){
+        group.cards[i]["display"] = "block"
+      }
+      nameData.push(group)
+    }
+  this.setData({
+        nameData:nameData
+  })
+  console.log('---------')
+  console.log(nameData)
   },
   onReady:function(){
     // 页面渲染完成
     //wx.setNavitionBarTitle('一个测试Titlte')
+    console.log('ready')
   },
   onShow:function(){
     // 页面显示
